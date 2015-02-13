12bc4df7b1007d787447a93867cfde0c17e31a15
jredd <jredd46@gmail.com>
2015-02-12 23:46:53 -0600

    cleaned up the imports in the main file

--- src/address_book/Main.java
+++ src/address_book/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -21,7 +20,6 @@ public class Main extends Application {
         scene.getStylesheets().add(css);
         primary_stage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
